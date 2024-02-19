6cae3278d873fc845162fdd922092e3ddaea96ee
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-02-19 14:50:52 +0530

    added images to my page

---
+++ image-sider/script.js
@@ -0,0 +1,38 @@
+const slider = document.querySelector(".slider");
+const dotsContainer=document.querySelector('.dots-container');
+
+
+async function fetchListOfImages() {
+  try {
+    const response = await fetch(
+      "https://picsum.photos/v2/list?page=1&limit=5",
+      {
+        method: "GET",
+      }
+    );
+    const imagesList = await response.json();
+    if (imagesList && imagesList.length > 0) displayImages(imagesList);
+    console.log(imagesList);
+  } catch (error) {
+    console.log("error occred");
+  }
+}
+
+function displayImages(imagesList) {
+  slider.innerHTML = imagesList.map(
+    (item) => `
+    <div class="slider">
+    <img src=${item.download_url} alt=${item.id}>
+    ${console.log(item.download_url)}
+    </div>
+    `
+  ).join(" ");
+
+  dotsContainer.innerHTML=imagesList.map(
+    (item,index)=>`
+    <span class="dot" data-slide="${index}"></span>
+    `
+  ).join(" ");
+}
+
+fetchListOfImages();
