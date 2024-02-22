8e7ec67b1ee5e45a3400843433c57f471195674a
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-02-22 15:31:40 +0530

    error occured

--- image-sider/script.js
+++ image-sider/script.js
@@ -1,42 +1,45 @@
 const slider = document.querySelector(".slider");
-const dotsContainer=document.querySelector('.dots-container');
-const image=document.querySelector(".images")
+const dotContainer=document.querySelector('.dots-container')
 
 async function fetchListOfImages() {
   try {
+    console.log('inside try block');
     const response = await fetch(
       "https://picsum.photos/v2/list?page=1&limit=5",
       {
         method: "GET",
       }
     );
+   
     const imagesList = await response.json();
-    if (imagesList && imagesList.length > 0) displayImages(imagesList);
     console.log(imagesList);
+
+    if (imagesList && imagesList.length > 0) {
+      displayImages(imagesList);
+    }
   } catch (error) {
-    console.log("error occred");
+    console.log(error);
   }
 }
 
-function displayImages(imagesList) {
-  slider.innerHTML = imagesList.map(
+function displayImages(getImagesList) {
+  slider.innerHTML = getImagesList.map(
     (item) => `
-    <div class="slide">
-    <img src=${item.download_url} alt=${item.id}>
-    ${console.log(item.download_url)}
+    <div class="slider">
+     <img src=${item.download_url} alt=${item.id}>
+     ${console.log(item.download_url)}
     </div>
     `
-  ).join(" ");
+    
+  );
 
-  dotsContainer.innerHTML=imagesList.map(
+  dotContainer.innerHTML=getImagesList.map(
     (item,index)=>`
-    <span class="dot" data-slide="${index}"></span>
+    <span class="dot active" data-slide=${index}></span>
     `
-  ).join(" ");
-
-
+    
+    ).join(" ");
+  
 }
 
 fetchListOfImages();
-
-
